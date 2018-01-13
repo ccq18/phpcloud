3a7ba295935fcd6a8b36aa3544cb23571e8def67
ccq18 <348578429@qq.com>
2018-01-14 00:03:11 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ $router->post('/services/register','IndexController@register');
 $router->get('/services','IndexController@allServices');
 $router->get('/services/{serviceName}','IndexController@service');
 $router->get('/',function (){
-    return 'lumen in docker';
+    return 'lumen in docker:'.__DIR__;
 });
 $router->get('/info',function (){
     phpinfo();
