7ad16feb955a446465f863cb959a3093b4d7eeb4
ccq18 <348578429@qq.com>
2018-01-19 01:23:41 +0800

    no message

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-chmod -R 777 storage
-php -d memory_limit=1G  /usr/local/bin/composer install
 docker run --rm -it -v $PWD:/www  ccq18/php-cli:7.1-v1 php /www/docker/install.php $*;
+chmod -R 777 storage;
+php -d memory_limit=1G  /usr/local/bin/composer install;
 (
 cd docker;
 docker-compose  -p phpcloud  stop;
