9d29bb33d62c9e2f4fe3a705c643dffe5628c136
ccq18 <348578429@qq.com>
2018-01-19 00:53:01 +0800

    no message

--- docker/install.php
+++ docker/install.php
@@ -2,5 +2,5 @@
 define('ROOT_PATH',__DIR__.'/../');
 $opt = getopt('', ['host:','port:','pwd:']);
 $s = file_get_contents(ROOT_PATH.'/.env.example');
-$s = str_replace(['{CENTER_HOST_VALUE}','{CENTER_PORT_VALUE}','{CENTER_PWD_VALUE}'],[$opt['host']?:"",$opt['port']?:"",$opt['pwd']?:""],$s);
+$s = str_replace(['{CENTER_HOST_VALUE}','{CENTER_PORT_VALUE}','{CENTER_PWD_VALUE}'],[$opt['hostip']?:"",$opt['port']?:"",$opt['pwd']?:""],$s);
 file_put_contents(ROOT_PATH.'/.env',$s);
