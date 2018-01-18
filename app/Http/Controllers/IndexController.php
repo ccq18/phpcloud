713cd4bea6f0daf9a068e81ff7e85a9851c6da70
ccq18 <348578429@qq.com>
2018-01-19 00:04:15 +0800

    no message

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -44,6 +44,10 @@ class IndexController extends Controller
      */
     public function service($serviceName)
     {
+        $base = $this->baseService();
+        if (isset($base[$serviceName])) {
+            return $base[$serviceName];
+        }
         return Service::firstOrNew($serviceName)
             ->getServers(Service::ACTIVE);
     }
@@ -54,10 +58,43 @@ class IndexController extends Controller
      */
     public function allServices()
     {
-        $services = [];
+        $services = $this->baseService();
         Service::all()->map(function (Service $s, $key) use (&$services) {
             $services[$key] = $s->getServers(Service::ACTIVE);
         });
         return $services;
+    }
+
+    public function baseService()
+    {
+        $s = [];
+        $location = '47.104.22.217:3306';
+        $s['db'] = [
+            $location => [
+                'serverLocation' => $location,
+                'healthUrl' => '',
+                'data' => [
+                    'DB_PORT' => 3306,
+                    'DB_HOST' => '47.104.22.217',
+                    'DB_USERNAME' => 'root',
+                    'DB_PASSWORD' => 'anneng',
+                ],
+                'status' => Service::ACTIVE
+            ]
+        ];
+        $location = env('REDIS_HOST') . ':' . env('REDIS_PORT');
+        $s['redis'] = [
+            $location => [
+                'serverLocation' => $location,
+                'healthUrl' => '',
+                'data' => [
+                    'REDIS_HOST' => env('REDIS_HOST'),
+                    'REDIS_PASSWORD' => env('REDIS_PASSWORD'),
+                    'REDIS_PORT' => env('REDIS_PORT'),
+                ],
+                'status' => Service::ACTIVE
+            ]
+        ];
+
     }
 }
