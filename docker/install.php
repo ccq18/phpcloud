67a7d2ac3e4bfb52ae8aac2acc564e0b8d85f27e
ccq18 <348578429@qq.com>
2018-01-19 09:56:23 +0800

    no message

--- docker/install.php
+++ docker/install.php
@@ -3,4 +3,5 @@ define('ROOT_PATH',__DIR__.'/../');
 $opt = getopt('', ['host:','port:','pwd:']);
 $s = file_get_contents(ROOT_PATH.'/.env.example');
 $s = str_replace(['{CENTER_HOST_VALUE}','{CENTER_PORT_VALUE}','{CENTER_PWD_VALUE}'],[$opt['hostip']?:"",$opt['port']?:"",$opt['pwd']?:""],$s);
-file_put_contents(ROOT_PATH.'/.env',$s);
+$rs = file_put_contents(ROOT_PATH.'/.env',$s);
+var_dump($s,ROOT_PATH.'/.env',$s,$rs);
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 docker run --rm -it -v $PWD:/www  ccq18/php-cli:7.1-v1 php /www/docker/install.php $*;
-chmod -R 777 storage;
-php -d memory_limit=1G  /usr/local/bin/composer install;
-(
-cd docker;
-docker-compose  -p phpcloud  stop;
-docker-compose -p phpcloud up --build -d;
-#docker system prune -f;
-)
+#chmod -R 777 storage;
+#php -d memory_limit=1G  /usr/local/bin/composer install;
+#(
+#cd docker;
+#docker-compose  -p phpcloud  stop;
+#docker-compose -p phpcloud up --build -d;
+##docker system prune -f;
+#)
