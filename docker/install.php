f584f1bb62607ab0bb5c73c01a3341bf6d2ab09b
ccq18 <348578429@qq.com>
2018-01-21 22:41:05 +0800

    no message

--- docker/install.php
+++ docker/install.php
@@ -1,14 +1,15 @@
 <?php
-define('ROOT_PATH', __DIR__ . '/../');
+define('ROOT_PATH',__DIR__.'/../');
 $centerIp = getenv('CENTER_IP');
 $centerPwd = getenv('CENTER_PWD');
 $centerPort = getenv('CENTER_PORT');
-$center = getenv('CENTER');;
-$s = file_get_contents(ROOT_PATH . '/.env.example');
-$s = str_replace(
-    ['{CENTER_HOST_VALUE}', '{CENTER_PORT_VALUE}', '{CENTER_PWD_VALUE}'],
-    [$centerIp, $centerPort, $centerPwd],
-    $s
-);
-$rs = file_put_contents(ROOT_PATH . '/.env', $s);
-var_dump(    [$centerIp, $centerPort, $centerPwd]);
+$center= getenv('CENTER');
+;
+
+
+
+$opt = getopt('', ['center_ip:','center_port:','center_pwd:']);
+$s = file_get_contents(ROOT_PATH.'/.env.example');
+$s = str_replace(['{CENTER_HOST_VALUE}','{CENTER_PORT_VALUE}','{CENTER_PWD_VALUE}'],[$opt['hostip']?:"",$opt['port']?:"",$opt['pwd']?:""],$s);
+$rs = file_put_contents(ROOT_PATH.'/.env',$s);
+var_dump($s,ROOT_PATH.'/.env',$s,$rs);
