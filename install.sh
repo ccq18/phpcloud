109c2207803dcbc69522215fa8114e2c7c1756bd
ccq18 <348578429@qq.com>
2018-01-21 23:38:49 +0800

    no message

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+php docker/install.php $*;
 docker run --rm -i -v $PWD:/www  ccq18/php-cli:7.1-v1 php /www/docker/install.php $*;
 chmod -R 777 storage;
 php -d memory_limit=1G  /usr/local/bin/composer install;
