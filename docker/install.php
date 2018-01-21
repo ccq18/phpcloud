76d72850cc84e1b6aaeb4ba290df2265b5a9365a
ccq18 <348578429@qq.com>
2018-01-21 22:55:46 +0800

    no message

--- docker/install.php
+++ docker/install.php
@@ -8,8 +8,8 @@ $center= getenv('CENTER');
 
 
 
-$opt = getopt('', ['center_ip:','center_port:','center_pwd:']);
+$opt = getopt('', ['center_ip:','center_port:','center_pwd:','hostip:']);
 $s = file_get_contents(ROOT_PATH.'/.env.example');
-$s = str_replace(['{CENTER_HOST_VALUE}','{CENTER_PORT_VALUE}','{CENTER_PWD_VALUE}'],[$opt['hostip']?:"",$opt['port']?:"",$opt['pwd']?:""],$s);
+$s = str_replace(['{CENTER_HOST_VALUE}','{CENTER_PORT_VALUE}','{CENTER_PWD_VALUE}'],[$opt['center_ip']?:"",$opt['center_port']?:"",$opt['center_pwd']?:""],$s);
 $rs = file_put_contents(ROOT_PATH.'/.env',$s);
 var_dump($s,ROOT_PATH.'/.env',$s,$rs);
